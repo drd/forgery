9c76c4b9e46c13c0800bd1797c556ac071d9dbe0
Eric O'Connell <eric.oconnell@plangrid.com>
2018-12-26 13:51:38 -0800

    Initial Commit

---
+++ mesher macOS/AppDelegate.swift
@@ -0,0 +1,30 @@
+//
+//  AppDelegate.swift
+//  mesher macOS
+//
+//  Created by Eric O'Connell on 12/26/18.
+//  Copyright © 2018 Eric O'Connell. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+    func applicationShouldTerminateAfterLastWindowClosed(_ sender: NSApplication) -> Bool {
+        return true
+    }
+
+
+}
+
